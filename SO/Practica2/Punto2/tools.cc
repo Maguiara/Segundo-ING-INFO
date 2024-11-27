d456999bdb1b8c0c5292ed21c9e961433222b345
Marco Aguiar Álvarez <alu0101620961@ull.edu.es>
2024-11-27 00:25:42 +0000

    Punto 1 acabado (Creo) y punto 2 full chaty

--- SO/Practica2/Punto2/tools.cc
+++ SO/Practica2/Punto2/tools.cc
@@ -7,7 +7,8 @@ void show_help() {
   std::cout << "Uso: docserver [opciones] archivo\n"
             << "Opciones:\n"
             << "  -h, --help: Mostrar esta ayuda\n"
-            << "  -v, --verbose: Mostrar información adicional\n";
+            << "  -v, --verbose: Mostrar información adicional\n"
+            << "  -p <puerto>, --port <puerto>: Especificar el puerto TCP\n";
 }
 
 std::expected<OpcionesAdmitidas, ErrorCode> parse_args(int argc, char* argv[]) {
@@ -18,10 +19,14 @@ std::expected<OpcionesAdmitidas, ErrorCode> parse_args(int argc, char* argv[]) {
     if (*it == "-h" || *it == "--help") {
       options.show_help_flag = true;
     } else if (*it == "-v" || *it == "--verbose") {
-      options.verbose_flag = true;
+    } else if (*it == "-p" || *it == "--port") {
+      if (++it == arguments.end()) return std::unexpected(ErrorCode::MISSING_ARGUMENTS);
+      options.port = std::stoi(std::string(*it));
+    } else if (!it->starts_with("-")) {
     } else if (!it->starts_with("-")) {
       options.aditional_arguments.push_back(std::string(*it));
-    }else {
+  if (options.aditional_arguments.empty()) return std::unexpected(ErrorCode::MISSING_FILE);
+  if (options.port == 0) options.port = 8080; // Default port
       return std::unexpected(ErrorCode::UNKNOWN_OPTION);
     }
   }
@@ -62,7 +67,60 @@ std::expected<SafeMap, int> read_all(const std::string& path, bool verbose) {
   return map;
 }
 
-void send_response(std::string_view header, std::string_view body) {
+void start_server(int port, const std::string& filepath, bool verbose) {
+  int server_fd, new_socket;
+  struct sockaddr_in address{};
+  int opt = 1;
+  int addrlen = sizeof(address);
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(port);
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+  if (listen(server_fd, 3) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+  while (true) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+      perror("accept");
+      continue;
+    }
+    // Leer el contenido del archivo
+    auto result = read_all(filepath, verbose);
+    if (!result) {
+      int err = result.error();
+      std::string response;
+      if (err == EACCES) {
+        response = "HTTP/1.1 403 Forbidden\n\n";
+      } else if (err == ENOENT) {
+        response = "HTTP/1.1 404 Not Found\n\n";
+      } else {
+        response = "HTTP/1.1 500 Internal Server Error\n\n";
+      }
+      send(new_socket, response.c_str(), response.size(), 0);
+    } else {
+      SafeMap file_map = result.value();
+      std::ostringstream response;
+      response << "HTTP/1.1 200 OK\n" << "Content-Length: " << file_map.get().size() << "\n\n" << file_map.get();
+      std::string response_str = response.str();
+      send(new_socket, response_str.c_str(), response_str.size(), 0);
+    }
+    close(new_socket);
+  }
+}
+
+std::string get_env(const std::string& name) {
   std::cout << header << "\n" << body << "\n";
 }
 
